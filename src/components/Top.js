a70d5dae2f4df7ffc6931e07caf4823e4822a97c
Surinder kumar <117210849+suri5567@users.noreply.github.com>
2023-04-09 16:18:01 +0530

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
+
 function Top() {
     const [cryptoArr, setCryptoArr] = useState([]);
+    useEffect(()=>{
+        fetch('https://api.coinlore.net/api/tickers/')
+            .then((res)=>{
+                return res.json();
+            })
+            .then((res)=>{
+                // console.log(res.data);
+                setCryptoArr(res.data);
+            })
+    },[])
 
     return (
         <div className='top-container'>
             <h1>Top 10 Global Crypto Coins</h1>
             <div className='display-container'>
-
-                <div className='coin-container'>
-                    <p className='rank-para'>Rank #rank</p>
-                    <h3 className='coin-symbol'>name (symbol)</h3>
-                    <p className='price-para'>Price: price_usd</p>
-                </div>
-
+                {   
+                    cryptoArr.slice(0,10).map((data)=>{
+                        
+                        return (
+                            <div className='coin-container'>
+                                <p className='rank-para'>Rank #{data.rank}</p>
+                                <h3 className='coin-symbol'>{data.name}({data.symbol})</h3>
+                                <p className='price-para'>Price: {data.price_usd}</p>
+                            </div>
+                        )
+                    })
+                }
+                
             </div>
         </div>
     )
